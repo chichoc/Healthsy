d33bb83f3fab5ccd3c30648b9b6f6d23a057fcaf
chichoc <kny6152@naver.com>
2022-02-18 20:03:09 +0900

    chore: Route 추가 및 분리

--- src/index.js
+++ src/index.js
@@ -4,28 +4,14 @@ import './index.css';
 import App from './App';
 import Global from './styles/global';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Help from './pages/Help';
-import Main from './pages/Main';
-import Join from './pages/Join';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import Sale from './pages/Sale';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/join' element={<Join />} />
-        <Route path='/sale' element={<Sale />} />
-        <Route path='/help' element={<Help />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <Global />
+      <App />
     </BrowserRouter>
-    <Global />
-    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
