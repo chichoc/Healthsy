2f588673744c8ff8483b7322d2b45cbf58033af9
chichoc <kny6152@naver.com>
2022-05-16 23:59:10 +0900

    chore: 상품 가격 3자리마다 쉼표 추가

--- front/src/components/SaleProduct.js
+++ front/src/components/SaleProduct.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 const SaleProduct = ({ brandName, productName, productPrice = '20,000' }) => {
+  const commaPrice = (price) => {
+    if (price < 1000) return price;
+    return price.toLocaleString();
+  };
+
   return (
     <>
       <img src='../images/testSale.jpeg' alt='제품 이미지' />
       <h1 className='productBrand'>{brandName}</h1>
       <h1 className='productName'>{productName}</h1>
-      <h2 className='productPrice'>{productPrice}원</h2>
+      <h2 className='productPrice'> {`${commaPrice(productPrice)}원`}</h2>
       {/* 헬씨 배송 여부 */}
     </>
   );
