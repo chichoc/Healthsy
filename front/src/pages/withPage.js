43e8b68b8b83eff7a4ccaa2e5651d1755e1ea8ee
chichoc <kny6152@naver.com>
2022-05-27 22:13:07 +0900

    chore: useLocation 적용 및 로그인 했을 때 회원가입페이지 접근 못하게 설정

--- front/src/pages/withPage.js
+++ front/src/pages/withPage.js
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { onLogIn } from '../store/features/page';
+import { onLogIn } from '../store/features/pageSlice';
 import axios from 'axios';
 import MainFooter from '../components/MainFooter';
 import MainHeader from '../components/MainHeader';
@@ -13,14 +13,18 @@ const withPage = (WrappedComponent) => {
     const page = useSelector((state) => state.page.value);
     const dispatch = useDispatch();
     const navigate = useNavigate();
+    const location = useLocation();
 
     useLayoutEffect(() => {
       if (!page.isLogin) {
         axios.post('http://localhost:8888/login/authorization', {}).then((res, req) => {
           if (!res.data.token) return;
-          if (res.data.updated) dispatch(onLogIn());
-          else {
-            if (window.location.pathname !== '/login') alert('로그인이 만료되어 다시 로그인 부탁드립니다');
+          if (res.data.updated) {
+            // 로그인 상태에서 회원가입 못하도록 설정
+            if (location.pathname === '/join') navigate('/');
+            dispatch(onLogIn());
+          } else {
+            if (location.pathname !== '/login') alert('로그인이 만료되어 다시 로그인 부탁드립니다');
             res.error && console.log(res.error);
             navigate('/login');
           }
