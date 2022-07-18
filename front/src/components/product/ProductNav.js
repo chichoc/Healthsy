192b4fd791150f99fb5b8afdbf577c6bfe5da124
chichoc <kny6152@naver.com>
2022-07-18 23:59:29 +0900

    chore: 상품 Nav 스크롤 지나칠 때 상단 고정

--- front/src/components/product/ProductNav.js
+++ front/src/components/product/ProductNav.js
@@ -1,13 +1,15 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import dataProductNav from '../../assets/api/dataProductNav';
 import { DivProduct, NavProduct, SectionProduct } from '../../styles/product/product_nav';
 import ProductDetail from './ProductDetail';
 import ProductInfo from './ProductInfo';
 import ProductReview from './ProductReview';
+import useSticky from '../customHook/useSticky';
 
 const ProductNav = () => {
   const [showComponent, setShowComponent] = useState(0);
+  const { sticky, stickyRef } = useSticky();
   const { reviews: countTotalReviews } = useSelector((state) => state.product.count);
   const infoSection = useRef(null);
   const detailSection = useRef(null);
@@ -21,7 +23,7 @@ const ProductNav = () => {
 
   return (
     <DivProduct>
-      <NavProduct className='horizontal_flex'>
+      <NavProduct className={sticky ? 'horizontal_flex sticky' : 'horizontal_flex'} ref={stickyRef}>
         {dataProductNav.map((nav, index) => (
           <button
             key={index}
